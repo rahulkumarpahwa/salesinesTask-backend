019f62bbc02a255bd1fd646cfebc48129973bf9f
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-06-18 09:43:11 +0530

    created new route for task which are done

---
+++ src/app/api/todo/donetask/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import Task from "@/models/taskModel";
+import { getDataFromToken } from "@/utils/getDataFromToken";
+import { connect } from "@/dbConfig/dbConfig";
+connect();
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const userId = getDataFromToken(request);
+    const tasks = await Task.find({ creator: userId });
+    const doneTasks = tasks.filter((value) => value.isDone === true);
+    return NextResponse.json({ success: true, tasks: doneTasks }, { status: 200 });
+  } catch (error: any) {
+    console.log(error.message);
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+};
